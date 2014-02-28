b5d638303d7b62b68b3ecae6f95cd63a3814a529
Simon Escobar Benitez <s.escob.b.27@gmail.com>
2014-02-27 23:56:51 -0500

    Index out of bounds fixed, and print function

--- src/triki/board.go
+++ src/triki/board.go
@@ -2,58 +2,50 @@ package triki
 
 import (
 	"errors"
-	"fmt"
+	"math"
 )
 
 type Board struct {
-	board   [9]string
-	Player1 Player
-	Player2 Player
+	Tboard    [9]string
+	Available int
 }
 
 func NewBoard() *Board {
-	return makeBoard()
-}
-
-func makeBoard() *Board {
-	b := &Board{board: [9]string{}}
-	for x := 0; x < len(b.board); x++ {
-		b.board[x] = E
-		fmt.Println(b.board[x])
+	b := &Board{Tboard: [9]string{},
+		Available: 9}
+	for x := 0; x < 9; x++ {
+		b.Tboard[x] = E
 	}
-	fmt.Print("Error: ")
-	fmt.Println(b)
 	return b
 }
 
 func (b *Board) CheckWin(p *Player) bool {
-	return (((b.board[0] == p.Symbol) && (b.board[1] == p.Symbol) && (b.board[2] == p.Symbol)) ||
-		((b.board[0] == p.Symbol) && (b.board[4] == p.Symbol) && (b.board[8] == p.Symbol)) ||
-		((b.board[0] == p.Symbol) && (b.board[3] == p.Symbol) && (b.board[6] == p.Symbol)) ||
-		((b.board[1] == p.Symbol) && (b.board[4] == p.Symbol) && (b.board[7] == p.Symbol)) ||
-		((b.board[2] == p.Symbol) && (b.board[4] == p.Symbol) && (b.board[6] == p.Symbol)) ||
-		((b.board[2] == p.Symbol) && (b.board[5] == p.Symbol) && (b.board[8] == p.Symbol)) ||
-		((b.board[3] == p.Symbol) && (b.board[4] == p.Symbol) && (b.board[5] == p.Symbol)))
+	return (((b.Tboard[0] == p.Symbol) && (b.Tboard[1] == p.Symbol) && (b.Tboard[2] == p.Symbol)) ||
+		((b.Tboard[0] == p.Symbol) && (b.Tboard[4] == p.Symbol) && (b.Tboard[8] == p.Symbol)) ||
+		((b.Tboard[0] == p.Symbol) && (b.Tboard[3] == p.Symbol) && (b.Tboard[6] == p.Symbol)) ||
+		((b.Tboard[1] == p.Symbol) && (b.Tboard[4] == p.Symbol) && (b.Tboard[7] == p.Symbol)) ||
+		((b.Tboard[2] == p.Symbol) && (b.Tboard[4] == p.Symbol) && (b.Tboard[6] == p.Symbol)) ||
+		((b.Tboard[2] == p.Symbol) && (b.Tboard[5] == p.Symbol) && (b.Tboard[8] == p.Symbol)) ||
+		((b.Tboard[3] == p.Symbol) && (b.Tboard[4] == p.Symbol) && (b.Tboard[5] == p.Symbol)))
 }
 
-func (b *Board) String() string {
-	tmp := ""
-	pos := 0
-	for x := 0; x < 3; x++ {
-		for y := 0; y < 3; y++ {
-			pos++
-			tmp += b.board[pos] + " "
-		}
-		tmp += "\n"
-	}
-	return tmp
-}
-
-func (b *Board) play(player Player, pos int) error {
-	if b.board[pos] == E {
-		b.board[pos] = player.Symbol
+func (b *Board) Play(player *Player, posx, posy float64) error {
+	pos := int(math.Abs(posx - posy))
+	if b.Tboard[pos] == E {
+		b.Tboard[pos] = player.Symbol
+		b.Available--
 		return nil
 	} else {
 		return errors.New("Invalid Position")
 	}
+}
+
+func (b *Board) Print() {
+	for i := 0; i < len(b.Tboard); i++ {
+		if i%3 == 0 {
+			println()
+		}
+		print(b.Tboard[i])
+	}
+	println()
 }
